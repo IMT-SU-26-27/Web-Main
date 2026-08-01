8d3db3c826c238875f009442af69d88d53ed3325
Lorelds <louisyuri2@gmail.com>
2026-08-01 23:10:13 +0700

    Update layout.tsx
    layout.tsx ke double

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,33 +26,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
-import "./globals.css";
-import Providers from "@/components/utils/Providers";
-import NavigationBar from "@/components/NavigationBar";
-import type { Metadata } from "next";
-import { pixelify } from '@/lib/utils/font';
-import Footer from "@/components/Footer";
-
-export const metadata: Metadata = {
-  title: "SU IMT UC",
-  description: "The Official Website of SU IMT UC",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${pixelify.variable} antialiased`} suppressHydrationWarning>
-        <Providers>
-          <NavigationBar />
-          {children}
-        </Providers>
-        <Footer></Footer>
-      </body>
-    </html>
-  );
-}
+}
